6846a8397a6ec28821e5395ad1dff3a477e6ac7c
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-05 15:29:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ShoppingStore from './components/Clothing'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ShoppingStore/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
