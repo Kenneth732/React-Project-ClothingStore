59ab5d57ecbbccc047cc96e481b00d6034f9a9a1
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-05 16:18:03 +0300

    done

--- src/components/Clothing.js
+++ src/components/Clothing.js
@@ -52,6 +52,7 @@ function ShoppingStore() {
       <div className="product-list">
         {products.map((product) => (
           <div key={product.id} className="product">
+            <img src={product.image} />
             <h3>{product.name}</h3>
             <p>${product.price}</p>
             <button onClick={() => addToCart(product)}>Add to Cart</button>
