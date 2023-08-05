d1d16946d215a62fdb9d5d2169c3b51142bed4a4
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-05 15:38:02 +0300

    setting state

--- src/components/Clothing.js
+++ src/components/Clothing.js
@@ -1,2 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './ShoppingStore.css';
+
+function ShoppingStore() {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+    const [isCartOpen, setIsCartOpen] = useState(false);
