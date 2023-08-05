813fe062b770ee8711db43b1836844059b1ec3bd
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-05 15:38:38 +0300

    setting useEffect for fetching data

--- src/components/Clothing.js
+++ src/components/Clothing.js
@@ -5,3 +5,9 @@ function ShoppingStore() {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
     const [isCartOpen, setIsCartOpen] = useState(false);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/clothingData')
+          .then((res) => res.json())
+          .then((data) => setProducts(data));
+      }, []);
