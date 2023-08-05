addd670d2c1f3b9cce685356a53ad401362d710a
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-05 15:43:19 +0300

    header

--- src/components/Clothing.js
+++ src/components/Clothing.js
@@ -2,41 +2,56 @@ import React, { useEffect, useState } from 'react';
 import './ShoppingStore.css';
 
 function ShoppingStore() {
-    const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([]);
-    const [isCartOpen, setIsCartOpen] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [isCartOpen, setIsCartOpen] = useState(false);
 
-    useEffect(() => {
-        fetch('http://localhost:3000/clothingData')
-          .then((res) => res.json())
-          .then((data) => setProducts(data));
-      }, []);
+  useEffect(() => {
+    fetch('http://localhost:3000/clothingData')
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
-      const addToCart = (product) => {
-        setCart([...cart, { ...product, quantity: 1 }]);
-      };
-    
-      const removeFromCart = (productId) => {
-        const updatedCart = cart.filter((item) => item.id !== productId);
-        setCart(updatedCart);
-      };
+  const addToCart = (product) => {
+    setCart([...cart, { ...product, quantity: 1 }]);
+  };
 
-      const increaseQuantity = (productId) => {
-        const updatedCart = cart.map((item) =>
-          item.id === productId ? { ...item, quantity: item.quantity + 1 } : item
-        );
-        setCart(updatedCart);
-      };
-    
-      const decreaseQuantity = (productId) => {
-        const updatedCart = cart.map((item) =>
-          item.id === productId && item.quantity > 1
-            ? { ...item, quantity: item.quantity - 1 }
-            : item
-        );
-        setCart(updatedCart);
-      };
-    
-      const toggleCart = () => {
-        setIsCartOpen(!isCartOpen);
-      };
+  const removeFromCart = (productId) => {
+    const updatedCart = cart.filter((item) => item.id !== productId);
+    setCart(updatedCart);
+  };
+
+  const increaseQuantity = (productId) => {
+    const updatedCart = cart.map((item) =>
+      item.id === productId ? { ...item, quantity: item.quantity + 1 } : item
+    );
+    setCart(updatedCart);
+  };
+
+  const decreaseQuantity = (productId) => {
+    const updatedCart = cart.map((item) =>
+      item.id === productId && item.quantity > 1
+        ? { ...item, quantity: item.quantity - 1 }
+        : item
+    );
+    setCart(updatedCart);
+  };
+
+  const toggleCart = () => {
+    setIsCartOpen(!isCartOpen);
+  };
+
+  return (
+    <div className="shopping-store">
+      <header>
+        <h1>Shopping Store</h1>
+        <button className="cart-button" onClick={toggleCart}>
+          Cart ({cart.reduce((totalItems, item) => totalItems + (item.quantity || 1), 0)})
+        </button>
+      </header>
+   
+    </div>
+  );
+}
+
+export default ShoppingStore;
